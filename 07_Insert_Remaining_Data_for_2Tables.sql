8b8a6d73c476b03946f61b5e15c6c6f26866afb2
Sahil Mokashi <101308350+Sahil-A-Mokashi@users.noreply.github.com>
2026-06-29 21:23:07 +0530

    Add and update return transactions in Returns table
    Inserted sample records into the Returns table as pending, approved the first 10, and rejected the last 5. Updated the wallet transactions table based on approved returns.

--- 07_Insert_Remaining_Data_for_2Tables.sql
+++ 07_Insert_Remaining_Data_for_2Tables.sql
@@ -144,3 +144,108 @@ FROM Wallets
 ORDER BY EmployeeID DESC;
 
 GO
+/*-------------------------------------------------------------------------------------
+    SImulate the returns to add transactions to the wallet transactions table, 
+    -insert all sample records into returns table as pending,
+    -approve the first 10 and reject last 5 ,
+    once we update the returns table a trigger will run, 
+    only for the records that were marked as approved the trigger will create a refund transaction in wallet transactions table.
+    
+    -----------------------------------------------------------------------------------------*/
+/*==========================================================
+Returns
+Insert Pending Returns
+==========================================================*/
+
+INSERT INTO Returns
+(
+    OrderID,
+    EmployeeID,
+    ApprovedBy,
+    ReturnReason,
+    RefundPoints,
+    ReturnStatus,
+    RequestDate,
+    ApprovalDate
+)
+SELECT TOP (20)
+    O.OrderID,
+    O.EmployeeID,
+    NULL,
+
+    CASE (O.OrderID % 5)
+        WHEN 0 THEN 'Damaged Product'
+        WHEN 1 THEN 'Wrong Item Delivered'
+        WHEN 2 THEN 'Changed Mind'
+        WHEN 3 THEN 'Defective Product'
+        ELSE 'Packaging Damage'
+    END,
+
+    CASE
+        WHEN O.PaymentMethod = 'Cash'
+            THEN 0
+
+        WHEN O.PaymentMethod = 'Points'
+            THEN CAST(O.TotalAmount * 10 AS INT)
+
+        WHEN O.PaymentMethod = 'Mixed'
+            THEN CAST((O.TotalAmount / 2.0) * 10 AS INT)
+    END,
+
+    'Pending',
+
+    DATEADD(DAY,5,O.OrderDate),
+
+    NULL
+
+FROM Orders O
+
+WHERE
+O.OrderStatus='Completed'
+
+ORDER BY
+O.OrderID;
+
+GO
+
+/*==========================================================
+Approve First 10 Returns
+==========================================================*/
+
+UPDATE Returns
+SET
+    ReturnStatus='Approved',
+    ApprovedBy=1,
+    ApprovalDate=GETDATE()
+
+WHERE ReturnID BETWEEN 1 AND 10;
+
+GO
+    
+/*==========================================================
+Reject Last 5 Returns
+==========================================================*/
+
+UPDATE Returns
+SET
+    ReturnStatus='Rejected',
+    ApprovedBy=1,
+    ApprovalDate=GETDATE()
+
+WHERE ReturnID BETWEEN 16 AND 20;
+
+GO
+
+
+/*-------------------------------------------------------------
+if only 10 records were approved then the wallet transactions table should only give 10 re ords, 
+
+SELECT
+TransactionType,
+TransactionSource,
+Points,
+OrderID
+FROM WalletTransactions
+WHERE TransactionType='Refund';
+
+------------------------------------------------------------*/
