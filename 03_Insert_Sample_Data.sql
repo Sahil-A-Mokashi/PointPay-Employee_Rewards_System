d6d05003e80976daf7f590ccddbfbeb2e17de50d
Sahil Mokashi <101308350+Sahil-A-Mokashi@users.noreply.github.com>
2026-07-14 06:52:08 +0530

    Refactor employee data and update order items
    Removed several employee records and updated their last login and failed attempts. Adjusted order items and their associated prices.

--- 03_Insert_Sample_Data.sql
+++ 03_Insert_Sample_Data.sql
@@ -5,6 +5,8 @@ Source:
 - U.S. Census Bureau - Frequently Occurring Surnames
 - U.S. Social Security Administration - Popular Baby Names
 
+pdf in the link -https://www.census.gov/topics/population/genealogy/data/2020_names.html
+
 Names are used only as fictional sample data for educational purposes.
 Initial password for all users: Password@123
 Stored using SHA2-256 hashing.
@@ -48,34 +50,7 @@ VALUES
 ('EMP022','Nancy Perez','nancy.perez@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
 ('EMP023','Daniel Thompson','daniel.thompson@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
 ('EMP024','Lisa White','lisa.white@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP025','Matthew Harris','matthew.harris@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP026','Betty Sanchez','betty.sanchez@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP027','Anthony Clark','anthony.clark@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP028','Margaret Ramirez','margaret.ramirez@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP029','Mark Lewis','mark.lewis@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP030','Sandra Robinson','sandra.robinson@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-
-('EMP031','Donald Walker','donald.walker@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP032','Ashley Young','ashley.young@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP033','Steven Allen','steven.allen@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP034','Kimberly King','kimberly.king@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP035','Paul Wright','paul.wright@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP036','Donna Scott','donna.scott@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP037','Andrew Torres','andrew.torres@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP038','Emily Nguyen','emily.nguyen@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP039','Joshua Hill','joshua.hill@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP040','Michelle Flores','michelle.flores@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-
-('EMP041','Kenneth Green','kenneth.green@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP042','Amanda Adams','amanda.adams@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP043','Kevin Nelson','kevin.nelson@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP044','Melissa Baker','melissa.baker@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP045','Brian Hall','brian.hall@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP046','Rebecca Rivera','rebecca.rivera@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP047','George Campbell','george.campbell@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP048','Stephanie Mitchell','stephanie.mitchell@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP049','Edward Carter','edward.carter@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2)),
-('EMP050','Amy Roberts','amy.roberts@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2));
+('EMP025','Matthew Harris','matthew.harris@pointpay.com',CONVERT(VARCHAR(64),HASHBYTES('SHA2_256','Password@123'),2));
 
 GO
 
@@ -93,17 +68,6 @@ UPDATE Employees
 SET LastLogin = DATEADD(DAY, -7, GETDATE())
 WHERE EmployeeID BETWEEN 11 AND 20;
 
-UPDATE Employees
-SET LastLogin = DATEADD(DAY, -15, GETDATE())
-WHERE EmployeeID BETWEEN 21 AND 30;
-
-UPDATE Employees
-SET LastLogin = DATEADD(DAY, -30, GETDATE())
-WHERE EmployeeID BETWEEN 31 AND 40;
-
-UPDATE Employees
-SET LastLogin = DATEADD(DAY, -60, GETDATE())
-WHERE EmployeeID BETWEEN 41 AND 50;
 
 
 -- Employees who recently entered incorrect passwords
@@ -113,26 +77,26 @@ WHERE EmployeeID IN (8, 19);
 
 UPDATE Employees
 SET FailedAttempts = 4
-WHERE EmployeeID = 27;
+WHERE EmployeeID = 20;
 
 
 -- One employee temporarily locked
 UPDATE Employees
 SET FailedAttempts = 5,
     LockUntil = DATEADD(MINUTE,15,GETDATE())
-WHERE EmployeeID = 46;
+WHERE EmployeeID = 14;
 
 
 -- Three employees have left the company
 UPDATE Employees
 SET IsActive = 0
-WHERE EmployeeID IN (13, 29, 47);
+WHERE EmployeeID IN (12);
 
 
 -- Employees with no previous login (new joiners)
 UPDATE Employees
 SET LastLogin = NULL
-WHERE EmployeeID IN (48,49,50);
+WHERE EmployeeID IN (15);
 
 GO
 
@@ -179,34 +143,7 @@ VALUES
 (22,'2026-05-02','2026-05-02'),
 (23,'2026-05-03','2026-05-03'),
 (24,'2026-05-04','2026-05-04'),
-(25,'2026-05-05','2026-05-05'),
-(26,'2026-05-06','2026-05-06'),
-(27,'2026-05-07','2026-05-07'),
-(28,'2026-05-08','2026-05-08'),
-(29,'2026-05-09','2026-05-09'),
-(30,'2026-05-10','2026-05-10'),
-
-(31,'2026-05-15','2026-05-15'),
-(32,'2026-05-16','2026-05-16'),
-(33,'2026-05-17','2026-05-17'),
-(34,'2026-05-18','2026-05-18'),
-(35,'2026-05-19','2026-05-19'),
-(36,'2026-05-20','2026-05-20'),
-(37,'2026-05-21','2026-05-21'),
-(38,'2026-05-22','2026-05-22'),
-(39,'2026-05-23','2026-05-23'),
-(40,'2026-05-24','2026-05-24'),
-
-(41,'2026-06-01','2026-06-01'),
-(42,'2026-06-02','2026-06-02'),
-(43,'2026-06-03','2026-06-03'),
-(44,'2026-06-04','2026-06-04'),
-(45,'2026-06-05','2026-06-05'),
-(46,'2026-06-06','2026-06-06'),
-(47,'2026-06-07','2026-06-07'),
-(48,'2026-06-08','2026-06-08'),
-(49,'2026-06-09','2026-06-09'),
-(50,'2026-06-10','2026-06-10');
+(25,'2026-05-05','2026-05-05');
 
 GO
 
@@ -229,6 +166,8 @@ rewards platforms and retail catalogues including:
 
 Prices are approximate retail values and are used only
 for educational purposes.
+
+link to the dataset- https://www.kaggle.com/datasets/supratimnag06/shop-product-catalog
 ==========================================================*/
 
 INSERT INTO Products
@@ -262,40 +201,7 @@ VALUES
 ('Dell 24-inch Monitor','PP017',179.00,1790,16,1),
 ('Anker USB-C Hub','PP018',55.00,550,38,1),
 ('Moleskine Notebook Set','PP019',25.00,250,60,1),
-('Hydro Flask Water Bottle','PP020',35.00,350,50,1),
-
-('Amazon Gift Card €25','PP021',25.00,250,120,1),
-('Amazon Gift Card €50','PP022',50.00,500,100,1),
-('Apple Gift Card €100','PP023',100.00,1000,80,1),
-('Google Play Gift Card','PP024',30.00,300,75,1),
-('Spotify Premium 12 Months','PP025',120.00,1200,40,1),
-('Netflix Gift Card','PP026',60.00,600,55,1),
-('Disney+ Annual Subscription','PP027',90.00,900,30,1),
-('Fitbit Inspire 3','PP028',99.00,990,18,1),
-('Apple Watch SE','PP029',299.00,2990,12,1),
-('Samsung Galaxy Buds FE','PP030',89.00,890,25,1),
-
-('LEGO Creator Set','PP031',65.00,650,22,1),
-('Nintendo Switch Controller','PP032',69.00,690,28,1),
-('Xbox Wireless Controller','PP033',64.00,640,26,1),
-('PlayStation DualSense Controller','PP034',74.00,740,20,1),
-('Coffee Hamper','PP035',35.00,350,40,1),
-('Tea Hamper','PP036',32.00,320,38,1),
-('Luxury Chocolate Gift Box','PP037',28.00,280,48,1),
-('Bluetooth Tracker Pack','PP038',55.00,550,20,0),
-('Portable Desk Fan','PP039',30.00,300,35,1),
-('Wireless Charging Stand','PP040',45.00,450,27,1),
-
-('Laptop Stand','PP041',39.00,390,45,1),
-('Desk Organizer Kit','PP042',25.00,250,50,1),
-('Smart LED Desk Lamp','PP043',55.00,550,25,1),
-('Bose SoundLink Flex','PP044',149.00,1490,14,1),
-('Instant Camera','PP045',89.00,890,15,1),
-('Travel Gift Set','PP046',45.00,450,24,0),
-('Premium Pen Set','PP047',55.00,550,30,1),
-('Wireless Presenter','PP048',42.00,420,28,1),
-('Noise Cancelling Earbuds','PP049',129.00,1290,16,1),
-('PointPay Premium Gift Hamper','PP050',150.00,1500,10,0);
+('Hydro Flask Water Bottle','PP020',35.00,350,50,1);
 
 GO
 /*-----------------------------------------------------------------------------------------------------------------------*/
@@ -332,107 +238,19 @@ VALUES
 (14,'ORD1014','Points','Completed','2026-04-16'),
 (15,'ORD1015','Mixed','Completed','2026-04-17'),
 
-(16,'ORD1016','Cash','Completed','2026-04-18'),
-(17,'ORD1017','Points','Completed','2026-04-19'),
-(18,'ORD1018','Mixed','Completed','2026-04-20'),
-(19,'ORD1019','Cash','Completed','2026-04-21'),
-(20,'ORD1020','Points','Completed','2026-04-22'),
-
-(21,'ORD1021','Mixed','Completed','2026-04-23'),
-(22,'ORD1022','Cash','Completed','2026-04-24'),
-(23,'ORD1023','Points','Completed','2026-04-25'),
-(24,'ORD1024','Mixed','Completed','2026-04-27'),
-(25,'ORD1025','Cash','Completed','2026-04-28'),
-
-(26,'ORD1026','Points','Completed','2026-04-29'),
-(27,'ORD1027','Cash','Completed','2026-04-30'),
-(28,'ORD1028','Mixed','Completed','2026-05-01'),
-(29,'ORD1029','Cash','Completed','2026-05-02'),
-(30,'ORD1030','Points','Completed','2026-05-03'),
-
-(31,'ORD1031','Mixed','Completed','2026-05-04'),
-(32,'ORD1032','Cash','Completed','2026-05-05'),
-(33,'ORD1033','Points','Completed','2026-05-06'),
-(34,'ORD1034','Mixed','Completed','2026-05-07'),
-(35,'ORD1035','Cash','Completed','2026-05-08'),
-
-(36,'ORD1036','Points','Completed','2026-05-09'),
-(37,'ORD1037','Mixed','Completed','2026-05-10'),
-(38,'ORD1038','Cash','Completed','2026-05-11'),
-(39,'ORD1039','Points','Completed','2026-05-12'),
-(40,'ORD1040','Mixed','Completed','2026-05-13'),
-
-(41,'ORD1041','Cash','Pending','2026-05-14'),
-(42,'ORD1042','Points','Pending','2026-05-15'),
-(43,'ORD1043','Mixed','Pending','2026-05-16'),
-(44,'ORD1044','Cash','Pending','2026-05-17'),
-(45,'ORD1045','Points','Pending','2026-05-18'),
-
-(46,'ORD1046','Mixed','Pending','2026-05-19'),
-(47,'ORD1047','Cash','Pending','2026-05-20'),
-(48,'ORD1048','Points','Pending','2026-05-21'),
-(49,'ORD1049','Mixed','Pending','2026-05-22'),
-(50,'ORD1050','Cash','Pending','2026-05-23'),
-
-(1,'ORD1051','Points','Completed','2026-05-24'),
-(2,'ORD1052','Mixed','Completed','2026-05-25'),
-(3,'ORD1053','Cash','Completed','2026-05-26'),
-(4,'ORD1054','Points','Completed','2026-05-27'),
-(5,'ORD1055','Mixed','Completed','2026-05-28'),
-
-(6,'ORD1056','Cash','Completed','2026-05-29'),
-(7,'ORD1057','Points','Completed','2026-05-30'),
-(8,'ORD1058','Mixed','Completed','2026-05-31'),
-(9,'ORD1059','Cash','Completed','2026-06-01'),
-(10,'ORD1060','Points','Completed','2026-06-02'),
-
-(11,'ORD1061','Mixed','Completed','2026-06-03'),
-(12,'ORD1062','Cash','Completed','2026-06-04'),
-(13,'ORD1063','Points','Completed','2026-06-05'),
-(14,'ORD1064','Mixed','Completed','2026-06-06'),
-(15,'ORD1065','Cash','Completed','2026-06-07'),
-
-(16,'ORD1066','Points','Completed','2026-06-08'),
-(17,'ORD1067','Mixed','Completed','2026-06-09'),
-(18,'ORD1068','Cash','Completed','2026-06-10'),
-(19,'ORD1069','Points','Completed','2026-06-11'),
-(20,'ORD1070','Mixed','Completed','2026-06-12'),
-
 (21,'ORD1071','Cash','Cancelled','2026-06-13'),
 (22,'ORD1072','Points','Cancelled','2026-06-14'),
 (23,'ORD1073','Mixed','Cancelled','2026-06-15'),
 (24,'ORD1074','Cash','Cancelled','2026-06-16'),
 (25,'ORD1075','Points','Cancelled','2026-06-17'),
 
-(26,'ORD1076','Cash','Completed','2026-06-18'),
-(27,'ORD1077','Points','Completed','2026-06-18'),
-(28,'ORD1078','Mixed','Completed','2026-06-19'),
-(29,'ORD1079','Cash','Completed','2026-06-19'),
-(30,'ORD1080','Points','Completed','2026-06-20'),
+(21,'ORD1091','Cash','Pending','2026-06-25'),
+(22,'ORD1092','Points','Pending','2026-06-26'),
+(23,'ORD1093','Mixed','Pending','2026-06-26'),
+(24,'ORD1094','Cash','Pending','2026-06-27'),
+(25,'ORD1095','Points','Pending','2026-06-27');
 
-(31,'ORD1081','Mixed','Completed','2026-06-20'),
-(32,'ORD1082','Cash','Completed','2026-06-21'),
-(33,'ORD1083','Points','Completed','2026-06-21'),
-(34,'ORD1084','Mixed','Completed','2026-06-22'),
-(35,'ORD1085','Cash','Completed','2026-06-22'),
 
-(36,'ORD1086','Points','Completed','2026-06-23'),
-(37,'ORD1087','Mixed','Completed','2026-06-23'),
-(38,'ORD1088','Cash','Completed','2026-06-24'),
-(39,'ORD1089','Points','Completed','2026-06-24'),
-(40,'ORD1090','Mixed','Completed','2026-06-25'),
-
-(41,'ORD1091','Cash','Pending','2026-06-25'),
-(42,'ORD1092','Points','Pending','2026-06-26'),
-(43,'ORD1093','Mixed','Pending','2026-06-26'),
-(44,'ORD1094','Cash','Pending','2026-06-27'),
-(45,'ORD1095','Points','Pending','2026-06-27'),
-
-(46,'ORD1096','Cash','Cancelled','2026-06-28'),
-(47,'ORD1097','Points','Cancelled','2026-06-28'),
-(48,'ORD1098','Mixed','Cancelled','2026-06-29'),
-(49,'ORD1099','Cash','Cancelled','2026-06-29'),
-(50,'ORD1100','Points','Cancelled','2026-06-30');
 
 GO
 
@@ -441,6 +259,7 @@ GO
 /*==========================================================
 INSERT DATA : OrderItems 
 ==========================================================*/
+
 
 INSERT INTO OrderItems
 (
@@ -464,14 +283,14 @@ VALUES
 (3,7,1,49.00,490),
 
 -- ORD1004
-(4,1,1,95.00,950),
+(4,4,1,119.00,1190),
 
 -- ORD1005
 (5,9,1,169.00,1690),
 
 -- ORD1006
-(6,4,1,119.00,1190),
-(6,6,1,45.00,450),
+(6,6,2,45.00,450),
+(6,8,1,59.00,590),
 
 -- ORD1007
 (7,5,1,39.00,390),
@@ -480,8 +299,8 @@ VALUES
 (8,11,1,129.00,1290),
 
 -- ORD1009
-(9,5,1,39.00,390),
-(9,6,1,45.00,450),
+(9,10,1,189.00,1890),
+(9,14,1,49.00,490),
 
 -- ORD1010
 (10,17,1,179.00,1790),
@@ -495,304 +314,45 @@ VALUES
 (12,20,1,35.00,350),
 
 -- ORD1013
-(13,31,1,65.00,650),
+(13,13,1,119.00,1190),
 
 -- ORD1014
-(14,25,1,120.00,1200),
+(14,12,1,109.00,1090),
 
 -- ORD1015
-(15,44,1,149.00,1490),
+(15,16,1,250.00,2500),
 
--- ORD1016
-(16,45,1,89.00,890),
+-- ORD1071
+(16,5,1,39.00,390),
 
--- ORD1017
-(17,1,1,95.00,950),
+-- ORD1072
+(17,8,1,59.00,590),
 
--- ORD1018
-(18,16,1,200.00,2000),
+-- ORD1073
+(18,15,2,20.00,200),
 
--- ORD1019
-(19,8,1,59.00,590),
+-- ORD1074
+(19,6,1,45.00,450),
+(19,2,1,42.00,420),
 
--- ORD1020
-(20,11,1,129.00,1290),
+-- ORD1075
+(20,4,1,119.00,1190),
 
--- ORD1021
-(21,22,1,50.00,500),
-(21,35,1,35.00,350),
+-- ORD1091
+(21,3,1,109.00,1090),
 
--- ORD1022
-(22,2,1,42.00,420),
+-- ORD1092
+(22,18,1,55.00,550),
 
--- ORD1023
-(23,27,1,90.00,900),
+-- ORD1093
+(23,1,1,95.00,950),
+(23,20,1,35.00,350),
 
--- ORD1024
-(24,10,1,189.00,1890),
-(24,5,1,39.00,390),
+-- ORD1094
+(24,7,1,49.00,490),
 
--- ORD1025
-(25,20,1,35.00,350),
-
--- ORD1026
-(26,18,1,55.00,550),
-
--- ORD1027
-(27,4,1,119.00,1190),
-
--- ORD1028
-(28,9,1,169.00,1690),
-(28,19,1,25.00,250),
-(28,20,1,35.00,350),
-
--- ORD1029
-(29,32,1,69.00,690),
-
--- ORD1030
-(30,23,1,100.00,1000),
-
--- ORD1031
-(31,7,1,49.00,490),
-(31,8,1,59.00,590),
-(31,42,1,25.00,250),
-
--- ORD1032
-(32,6,1,45.00,450),
-
--- ORD1033
-(33,16,1,250.00,2500),
-
--- ORD1034
-(34,5,1,39.00,390),
-(34,7,1,49.00,490),
-(34,40,1,45.00,450),
-
--- ORD1035
-(35,14,1,49.00,490),
-
--- ORD1036
-(36,1,1,95.00,950),
-
--- ORD1037
-(37,3,1,109.00,1090),
-(37,20,1,35.00,350),
-(37,42,1,25.00,250),
-
--- ORD1038
-(38,39,1,30.00,300),
-
--- ORD1039
-(39,3,1,109.00,1090),
-
--- ORD1040
-(40,1,1,95.00,950),
-(40,29,1,299.00,2990),
-
--- ORD1041
-(41,2,1,42.00,420),
-
--- ORD1042
-(42,10,1,189.00,1890),
-
--- ORD1043
-(43,1,1,95.00,950),
-(43,6,1,45.00,450),
-(43,15,1,20.00,200),
-
--- ORD1044
-(44,31,1,65.00,650),
-
--- ORD1045
-(45,50,1,150.00,1500),
-
--- ORD1046
-(46,44,1,149.00,1490),
-(46,20,1,35.00,350),
-
--- ORD1047
-(47,35,1,35.00,350),
-
--- ORD1048
-(48,11,1,129.00,1290),
-
--- ORD1049
-(49,3,1,109.00,1090),
-(49,6,1,45.00,450),
-(49,19,1,25.00,250),
-
--- ORD1050
-(50,8,1,59.00,590),
-
--- ORD1051
-(51,3,1,109.00,1090),
-
--- ORD1052
-(52,7,1,49.00,490),
-(52,8,1,59.00,590),
-(52,20,1,35.00,350),
-
--- ORD1053
-(53,14,1,49.00,490),
-
--- ORD1054
-(54,17,1,179.00,1790),
-
--- ORD1055
-(55,8,1,59.00,590),
-(55,11,1,129.00,1290),
-
--- ORD1056
-(56,35,1,35.00,350),
-
--- ORD1057
-(57,49,1,129.00,1290),
-
--- ORD1058
-(58,12,1,109.00,1090),
-(58,19,1,25.00,250),
-(58,20,1,35.00,350),
-
--- ORD1059
-(59,8,1,59.00,590),
-
--- ORD1060
-(60,9,1,169.00,1690),
-
--- ORD1061
-(61,4,1,119.00,1190),
-(61,6,1,45.00,450),
-
--- ORD1062
-(62,2,1,42.00,420),
-
--- ORD1063
-(63,10,1,189.00,1890),
-
--- ORD1064
-(64,46,1,45.00,450),
-(64,47,1,55.00,550),
-
--- ORD1065
-(65,45,1,89.00,890),
-
--- ORD1066
-(66,1,1,95.00,950),
-
--- ORD1067
-(67,16,1,250.00,2500),
-(67,15,1,20.00,200),
-
--- ORD1068
-(68,5,1,39.00,390),
-
--- ORD1069
-(69,26,1,60.00,600),
-
--- ORD1070
-(70,1,1,95.00,950),
-(70,2,1,42.00,420),
-(70,6,1,45.00,450),
-
--- ORD1071 (Cancelled)
-(71,21,1,25.00,250),
-
--- ORD1072 (Cancelled)
-(72,27,1,90.00,900),
-
--- ORD1073 (Cancelled)
-(73,3,1,109.00,1090),
-(73,6,1,45.00,450),
-
--- ORD1074 (Cancelled)
-(74,31,1,65.00,650),
-
--- ORD1075 (Cancelled)
-(75,29,1,299.00,2990),
-
--- ORD1076 (Completed)
-(76,3,1,109.00,1090),
-
--- ORD1077 (Completed)
-(77,10,1,189.00,1890),
-
--- ORD1078 (Completed)
-(78,6,1,45.00,450),
-(78,7,1,49.00,490),
-(78,20,1,35.00,350),
-
--- ORD1079 (Completed)
-(79,18,1,55.00,550),
-
--- ORD1080 (Completed)
-(80,9,1,169.00,1690),
-
--- ORD1081
-(81,46,1,45.00,450),
-(81,47,1,55.00,550),
-
--- ORD1082
-(82,35,1,35.00,350),
-
--- ORD1083
-(83,11,1,129.00,1290),
-
--- ORD1084
-(84,1,1,95.00,950),
-(84,2,1,42.00,420),
-
--- ORD1085
-(85,2,1,42.00,420),
-
--- ORD1086
-(86,29,1,299.00,2990),
-
--- ORD1087
-(87,3,1,109.00,1090),
-(87,5,1,39.00,390),
-(87,6,1,45.00,450),
-
--- ORD1088
-(88,8,1,59.00,590),
-
--- ORD1089
-(89,25,1,120.00,1200),
-
--- ORD1090
-(90,4,1,119.00,1190),
-(90,6,1,45.00,450),
-
--- ORD1091 (Pending)
-(91,1,1,95.00,950),
-
--- ORD1092 (Pending)
-(92,45,1,89.00,890),
-
--- ORD1093 (Pending)
-(93,16,1,250.00,2500),
-(93,15,1,20.00,200),
-
--- ORD1094 (Pending)
-(94,14,1,49.00,490),
-
--- ORD1095 (Pending)
-(95,49,1,129.00,1290),
-
--- ORD1096 (Cancelled)
-(96,6,1,45.00,450),
-
--- ORD1097 (Cancelled)
-(97,44,1,149.00,1490),
-
--- ORD1098 (Cancelled)
-(98,10,1,189.00,1890),
-(98,19,1,25.00,250),
-
--- ORD1099 (Cancelled)
-(99,5,1,39.00,390),
-
--- ORD1100 (Cancelled)
-(100,16,1,250.00,2500);
+-- ORD1095
+(25,10,1,189.00,1890);
 
 GO
 
